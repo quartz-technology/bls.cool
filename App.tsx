0bc758ba1523caf3eca0b4d58e082c338d33643c
Luca Georges Francois <luca@quartz.technology>
2022-10-10 16:13:41 -0700

    fix: update wrong homepage
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- App.tsx
+++ App.tsx
@@ -1,19 +1,9 @@
 import React from "react";
 import {
-  Text,
-  HStack,
   Center,
-  Switch,
-  useColorMode,
   NativeBaseProvider,
 } from "native-base";
 import SignatureVerifier from "./components/SignatureVerifier";
-
-// Define the config
-const config = {
-  useSystemColorMode: false,
-  initialColorMode: "dark",
-};
 
 export default function App() {
     return (
@@ -22,23 +12,5 @@ export default function App() {
                 <SignatureVerifier />
             </Center>
         </NativeBaseProvider>
-  );
-}
-
-// Color Switch Component
-function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light"}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
   );
 }
